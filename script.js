7796e442d1de47bef3f93b65ef819a01dc7e8720
SalamunKawlam <114695241+SalamunKawlam@users.noreply.github.com>
2023-09-03 01:52:08 +0600

    v0.2 with continuous & oneshot scanning
    Added continuous scanning.
    Added toggle to switch between continuous and one-shot scanning modes.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const scanner = new Html5QrcodeScanner('reader',{qrbox: {width: 320, height: 240,}, fps: 30,});
+let type = document.getElementsByName('scanType');
 let count = 0;
 
 scanner.render(success, error);
@@ -6,17 +7,26 @@ scanner.render(success, error);
 function success(result) {
     console.log(result);
     count = count+1;
-    if (count == 1){
-        console.log(count);
-        document.getElementById('result').innerHTML = `<h3 class="success">success</h3> <a href="${result}">${result}</a> <p>${count}</p>`;
-        playaudio();
+
+    if (type[1].checked){
+        if (count == 1){
+            console.log(count);
+            document.getElementById('result').innerHTML = `<h3 class="success">QR Detected Successfully</h3> <a href="${result}">${result}</a> <p>${count}</p>`;
+            playaudio();
+            }
         }
+        
+    else if (type[0].checked){
+        console.log(count);
+        document.getElementById('result').innerHTML = `<h3 class="success">QR Detected Successfully</h3> <a href="${result}">${result}</a> <p>${count}</p>`;
+        playaudio();
+    }  
 }
 
 function error(err) {
-    console.error(err);
+    console.log(err);
     count = 0;
-    document.getElementById('result').innerHTML = `<h3 class="error">QR not found/invalid</h3>`;
+    document.getElementById('result').innerHTML = `<h3 class="error">QR Not Found</h3>`;
 }
 
 function playaudio() {
