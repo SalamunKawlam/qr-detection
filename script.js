13f356ad3327b69ad7d27e05c9a2c9faba57beed
SalamunKawlam <114695241+SalamunKawlam@users.noreply.github.com>
2023-11-13 23:14:02 +0600

    Added Near Sense Functionality
    When Near Sense is enabled, the Scanner will try to sense presence using the ambient light sensor data and show/hide the scanner based on it.

--- script.js
+++ script.js
@@ -1,8 +1,43 @@
-const scanner = new Html5QrcodeScanner('reader',{qrbox: {width: 320, height: 240,}, fps: 30,});
-let type = document.getElementsByName('scanType');
-let count = 0;
+const near = document.getElementById('near');
+let result = document.getElementById('result');
+let nearSense = 0;
+// const scrWidth = screen.width;
+// const scrHeight = screen.height;
+// const minRes = Math.min(scrWidth, scrHeight);
+let scanRegion = document.getElementById("reader").offsetWidth;
 
-scanner.render(success, error);
+if ("AmbientLightSensor" in window) {
+    const sensor = new AmbientLightSensor();
+    sensor.addEventListener("reading", (event) => {
+        result.innerHTML = `<h3>Light Level: ${sensor.illuminance}</h3>`;
+        console.log("Current light level:", sensor.illuminance);
+    });
+    sensor.addEventListener("error", (event) => {
+      console.log(event.error.name, event.error.message);
+    });
+    sensor.start();
+}
+
+const scanner = new Html5QrcodeScanner('reader',{qrbox: {width: scanRegion*0.5, height: scanRegion*0.5}, fps: 30});
+let type = document.getElementsByName('scanType');
+check_status();
+
+function check_status(){
+    near.checked? nearSense=1: nearSense=0;
+    console.log(nearSense);
+    
+    if (nearSense==0){
+        scanner.render(success, error);
+    }
+
+    else if (nearSense==1) {
+        scanner.clear();
+        result.innerHTML = `<h3>Didn't Sense Anyone. On Standby...</h3>`;
+        result.innerHTML = `<h3 class="success">Detected Someone! Initializing Camera...</h3>`;
+        let count = 0;
+        scanner.render(success, error);
+    }
+}
 
 function success(result) {
     console.log(result);
@@ -11,25 +46,24 @@ function success(result) {
     if (type[1].checked){
         if (count == 1){
             console.log(count);
-            document.getElementById('result').innerHTML = `<h3 class="success">QR Detected Successfully</h3> <a href="${result}">${result}</a> <p>${count}</p>`;
+            result.innerHTML = `<h3 class="success">QR Detected Successfully</h3> <a href="${result}">${result}</a> <p>${count}</p>`;
             playaudio();
             }
         }
         
     else if (type[0].checked){
         console.log(count);
-        document.getElementById('result').innerHTML = `<h3 class="success">QR Detected Successfully</h3> <a href="${result}">${result}</a> <p>${count}</p>`;
+        result.innerHTML = `<h3 class="success">QR Detected Successfully</h3> <a href="${result}">${result}</a> <p>${count}</p>`;
         playaudio();
     }  
 }
 
 function error(err) {
-    console.log(err);
-    count = 0;
-    document.getElementById('result').innerHTML = `<h3 class="error">QR Not Found</h3>`;
+count = 0;
+result.innerHTML = `<h3 class="error">QR Not Found</h3>`;
 }
 
 function playaudio() {
-    let audio = new Audio("success.mp3");
-    audio.play();
+let audio = new Audio("success.mp3");
+audio.play();
 }
