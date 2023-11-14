0fe3c32aa1c36aa2df40441df4197816a58f665f
SalamunKawlam <114695241+SalamunKawlam@users.noreply.github.com>
2023-11-15 00:48:00 +0600

    Added Dynamic QR Scanner based on NearSense
    The QR Scanner will now trigger when presence is detected and Near Sense is triggered .

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+
 const near = document.getElementById('near');
 let result = document.getElementById('result');
 let nearSense = 0;
+let light = 0;
 // const scrWidth = screen.width;
 // const scrHeight = screen.height;
 // const minRes = Math.min(scrWidth, scrHeight);
@@ -8,10 +10,12 @@ let scanRegion = document.getElementById("reader").offsetWidth;
 
 if ("AmbientLightSensor" in window) {
     const sensor = new AmbientLightSensor();
+
     sensor.addEventListener("reading", (event) => {
-        result.innerHTML = `<h3>Light Level: ${sensor.illuminance}</h3>`;
-        console.log("Current light level:", sensor.illuminance);
+        light = sensor.illuminance;
+        // console.log("Light Level: "+light);
     });
+
     sensor.addEventListener("error", (event) => {
       console.log(event.error.name, event.error.message);
     });
@@ -20,24 +24,28 @@ if ("AmbientLightSensor" in window) {
 
 const scanner = new Html5QrcodeScanner('reader',{qrbox: {width: scanRegion*0.5, height: scanRegion*0.5}, fps: 30});
 let type = document.getElementsByName('scanType');
-check_status();
 
-function check_status(){
-    near.checked? nearSense=1: nearSense=0;
+function checkStatus(){
+    near.checked?nearSense =1 : nearSense = 0;
     console.log(nearSense);
     
-    if (nearSense==0){
+    if (nearSense == 0){
         scanner.render(success, error);
     }
 
-    else if (nearSense==1) {
+    else if (nearSense == 1) {
+        if (light < 200){
         scanner.clear();
         result.innerHTML = `<h3>Didn't Sense Anyone. On Standby...</h3>`;
-        result.innerHTML = `<h3 class="success">Detected Someone! Initializing Camera...</h3>`;
-        let count = 0;
-        scanner.render(success, error);
+        }
+        else if (light < 200){
+            result.innerHTML = `<h3 class="success">Sensed Someone! Initiating Scanner...</h3>`;
+            scanner.render(success, error);
+        }
     }
 }
+
+checkStatus();
 
 function success(result) {
     console.log(result);
@@ -59,11 +67,11 @@ function success(result) {
 }
 
 function error(err) {
-count = 0;
-result.innerHTML = `<h3 class="error">QR Not Found</h3>`;
+    count = 0;
+    result.innerHTML = `<h3 class="error">QR Not Found</h3>`;
 }
 
 function playaudio() {
-let audio = new Audio("success.mp3");
-audio.play();
+    let audio = new Audio("success.mp3");
+    audio.play();
 }
